7d0532d5b7a2660afd14009596d478a6ead4dcff
eduardo bravo <eduardo.bravo@geopagos.com>
2022-12-21 17:10:36 -0300

    CORS

--- ApiPeliculas/Program.cs
+++ ApiPeliculas/Program.cs
@@ -27,6 +27,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Soporte CORS
+//Se pueden habilitar: 1-Un dominio, 2-multiples dominios,
+//3-cualquier dominio (tener en cuenta seguridad)
+//Usames de ejemplo el dominio: http://localhost:3223, se debe cambiar por el correcto
+//Se usa (*) para todos los dominios
+builder.Services.AddCors(p => p.AddPolicy("PolicyCors", build =>
+{
+    build.WithOrigins("http://localhost:3223").AllowAnyMethod().AllowAnyHeader();
+}));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -37,6 +47,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+//Soporte para CORS
+app.UseCors("PolicyCors");
 
 app.UseAuthorization();
 
