c0e67c78af0eea82d117a7aca3b20212732f5d92
eduardo bravo <eduardo.bravo@geopagos.com>
2022-12-28 01:29:05 -0300

    RutaImagen type changed

--- ApiPeliculas/Modelos/Dtos/PeliculaDto.cs
+++ ApiPeliculas/Modelos/Dtos/PeliculaDto.cs
@@ -9,7 +9,7 @@ public class PeliculaDto
     
     [Required(ErrorMessage = "El nombre es obligatorio")] 
     public string Nombre { get; set; }
-    public string RutaImagen { get; set; }
+    public byte[] RutaImagen { get; set; }
     [Required(ErrorMessage = "La descripcion es obligatoria")] 
     public string Descripcion { get; set; }
     [Required(ErrorMessage = "La duracion es obligatoria")] 
